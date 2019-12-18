761b4096b3935fb17fc3b669de4c54c7a95c5423
Settlers <ssaquo@gmail.com>
2019-12-18 15:37:46 -0300

    Subindo a barra do menu estático feito

--- projeto-4used/src/App.js
+++ projeto-4used/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './components/Home'
-import ListaDeProduto from './components/ListaDeProduto/index'
 
+import ListaDeProduto from './components/ListaDeProduto/index'
 
 const produtos = [
   {
@@ -138,12 +138,12 @@ const produtos = [
   }
 ]
 
-
 function App() {
   return (
     <div className="App">
-        <Home/>
-        <ListaDeProduto/>
+      <Home/>
+
+      <ListaDeProduto/>
     </div>
   );
 }
